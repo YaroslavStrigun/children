574c30f127550dfa7428e56cfa0aac88c84d608e
Ярослав <yastrigun@gmail.com>
2018-10-29 21:53:50 +0200

    ADD: Post, Category models and migrations

---
+++ app/Models/Category.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $fillable = [
+        'name',
+        'title',
+        'keywords',
+        'description'
+    ];
+
+    public function posts()
+    {
+        return $this->hasMany('App\Models\Post', 'category_id');
+    }
+}
